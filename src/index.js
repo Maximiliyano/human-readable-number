809e26a93aa17a24fc02bbaf6195b034a23961b7
Maximiliyano <maxim24152771@gmail.com>
2022-01-09 01:53:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,17 @@
-var nOne = [ "", "one ", "two ", "three ", "four ",
-    "five ", "six ", "seven ", "eight ",
-     "nine ", "ten ", "eleven ", "twelve ",
-      "thirteen ", "fourteen ", "fifteen ",
-       "sixteen ", "seventeen ", "eighteen ",
-        "nineteen " ];
-
-var nTen = [ "", "", "twenty ", "thirty ", "forty ",
-"fifty ", "sixty ", "seventy ", "eighty ",
-        "ninety " ];
-
-function convertToWords(number, s)
-{
-
-    var arr = "";
-    if(number > 19)
-    {
-        arr = nTen[parseInt(number / 10)] + nOne[number % 10];
-    }
-    else
-    {
-        arr += nOne[number];
-    }
-    if (number != 0) 
-    {
-        arr += s;
-    }
-
-    return arr;
-}
+var nOne = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+var nTen = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
 
 module.exports = function toReadable (number) 
 {
-    var out = "";
+    if ((number = number.toString()).length > 9) return 'refull';
+    n = ('000000000' + number).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[0] == 0) ? (nOne[Number(n[0])] + 'zero') + '' : '';
+    str += (n[1] != 0) ? (nOne[Number(n[1])] + ' ' || nTen[n[1][0]] + ' ' + nOne[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (nOne[Number(n[2])] + ' ' || nTen[n[2][0]] + ' ' + nOne[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (nOne[Number(n[3])] + ' ' || nTen[n[3][0]] + ' ' + nOne[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (nOne[Number(n[4])] + ' ' || nTen[n[4][0]] + ' ' + nOne[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? (nOne[Number(n[5])] || nTen[n[5][0]] + ' ' + nOne[n[5][1]]) + '' : '';
 
-    out += convertToWords(parseInt(number / 10000000),
-    "crore");
-
-    out += convertToWords(parseInt((number / 100000) % 100),
-    "lakh");
-
-    out += convertToWords(parseInt((number / 1000) % 100),
-    "thousand");
-
-    out += convertToWords(parseInt((number / 100) % 10),
-    "hundred");
-
-    out += convertToWords(parseInt(number % 100), "");
-
-    return out;
+    return str.trim();
 }
